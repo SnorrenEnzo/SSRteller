e633bcda1367daf514a6f39e9ee46a527c68cece
SnorrenEnzo <31887537+SnorrenEnzo@users.noreply.github.com>
2017-12-24 19:31:33 +0100

    First commit

---
+++ SSR_teller_stats.py
@@ -0,0 +1,275 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 13 11:57:12 2016
+
+@author: Jelle Mes
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+import seaborn as sns
+#for downloading
+from urllib.request import urlretrieve
+#for removing the downloaded file
+import os
+
+plt.close()
+
+
+#start and end time of borrel data
+starttime = dt.time(20)
+endtime = dt.time(5)
+#start and end time of plot
+startPlot = dt.time(19,30)
+endPlot = dt.time(2,30)
+
+#earliest and latest start of counting
+earliest = dt.datetime.now()
+latest = dt.datetime(1900, 1, 1)
+
+#list of first and last data points for each day. From these the plotting
+#range is determined
+earliestlist = []
+latestlist = []
+
+downloadname = "./teller_log.txt"
+
+url = "https://api.joostvansomeren.nl/teller/log.txt"
+
+try:
+	#import data
+	data = np.loadtxt(downloadname, dtype = str).T
+	print("Loading old data. Remove 'Teller_log.txt' to download new data.")
+except:
+	#download log
+	print("Downloading new data")
+	urlretrieve(url, downloadname)
+	data = np.loadtxt(downloadname, dtype = str).T
+
+#remove downloaded file
+#os.remove(downloadname)
+
+#loads amount of people in the building in an array
+amount = data[0].astype(int)
+#converts month abbreviations to numbers
+monthN = np.zeros(len(data[3]), dtype = int)
+monthlist = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+for i in np.arange(len(data[3])):
+	tempmonth = data[3, i]
+	monthN[i] = monthlist.index(tempmonth) + 1
+
+#loads the date and time as datetime objects
+date = []
+time = []
+for i in np.arange(len(data[6])):
+	#combines the multiple data string arrays into single string
+	tempstring = str(monthN[i]) + '-' + data[4, i] + '-' + data[5, i] + ' ' + data[6, i]	
+	date = np.append(date, dt.datetime.strptime(tempstring, '%m-%d-%Y %H:%M:%S'))
+
+
+#slices the data to an array at a single day
+def sliceDates(checkDate, starttime, endtime, firstDay):
+	'''
+	Slices data to a single array if it is between the specified time interval
+	
+	Input:
+		checkDate (datetime): the date
+		
+		startime (datetime): the time before which all data points can be thrown
+		away
+		
+		endtime (datetime): the time after which all data points can be thrown 
+		away
+		
+		firstDay (boolean): whether we are looking at the first day of the event
+		(because events switch to a different day at 24:00 h)
+		
+	Returns:
+		slicedamount (int array): sliced array of amount values
+		
+		sliceddate (datetime array): sliced array containing the date & time
+	'''
+	slicedamount = []
+	sliceddate = []
+	
+	if firstDay == False:
+		 checkDate += dt.timedelta(days = 1)
+	
+	for i in np.arange(len(amount)):
+		if date[i].date() == checkDate and ((date[i].time() > starttime and 
+		firstDay) or (date[i].time() < endtime and firstDay == False)):
+			slicedamount = np.append(slicedamount, amount[i])
+			sliceddate = np.append(sliceddate, date[i])
+			
+	return slicedamount, sliceddate
+
+#.scatter(time, amount)
+#plt.show()
+
+#plots teller data of single day
+def plotsingleday(yr, m, d, colour, earliest, latest):
+	checkdate = dt.date(yr, m, d)
+	
+	#loads that day and the day after (because events go on past 24:00 h)
+	slicedamount1, slicedtime1 = sliceDates(checkdate, starttime, endtime, True)
+	slicedamount2, slicedtime2 = sliceDates(checkdate, starttime, endtime, False)
+	slicedamount = np.append(slicedamount1, slicedamount2)
+	#slicedtime = np.append(slicedtime1, slicedtime2)
+	#makes a datetime array which can be used for plotting (crossing 00:00 h)
+	sliceddate = []
+	for i in np.arange(len(slicedtime1)):
+		tempappend1 = dt.datetime.combine(dt.date(2016,5,1), slicedtime1[i].time())
+		sliceddate = np.append(sliceddate, tempappend1)
+	for i in np.arange(len(slicedtime2)):
+		tempappend2 = dt.datetime.combine(dt.date(2016,5,2), slicedtime2[i].time())
+		sliceddate = np.append(sliceddate, tempappend2)
+	#checks the earliest and latest times
+	if sliceddate[0] < earliest:
+		earliest = sliceddate[0]
+	if sliceddate[len(sliceddate) - 1] > latest:
+		latest = sliceddate[len(sliceddate) - 1]
+		
+	#print the latest 8 registrations
+	print("----------------")
+	print("Latest entries:")
+	for i in np.arange(8)[::-1]:
+		print(sliceddate[len(sliceddate) - i - 1].time(), slicedamount[len(sliceddate) - i - 1])
+	print("----------------")
+	
+	#plotting
+#	fig, ax = plt.subplots()
+	#plt.scatter(sliceddate, slicedamount, edgecolor = 'none', color = colour, label = checkdate)
+	plt.plot(sliceddate, slicedamount, color = colour, label = checkdate)
+	#plt.xlim(dt.datetime.combine(sliceddate[0].date(), startPlot), dt.datetime.combine(sliceddate[len(sliceddate)-1].date(), endPlot))
+	
+	#set the x ticks in appropriate positions
+#	xax = ax.get_xaxis() # get the x-axis
+#	adf = xax.get_major_formatter() # the the auto-formatter
+#	adf.scaled[1./24] = '%H:%M'  # set the < 1d scale to H:M
+#	adf.scaled[1.0] = '%Y-%m-%d' # set the > 1d < 1m scale to Y-m-d
+#	adf.scaled[30.] = '%Y-%m' # set the > 1m < 1Y scale to Y-m
+#	adf.scaled[365.] = '%Y' # set the > 1y scale to Y
+	
+	earliestlist.append(earliest)
+	latestlist.append(latest)
+	
+	#plot the most recent date
+def plotRecent(earliest, latest):
+	#first load the most recent date
+	#loop through dates starting at the most recent
+	rdate = date[::-1]
+	ramount = amount[::-1]
+	for i in np.arange(len(rdate)):
+		if rdate[i].time() > dt.time(12) and ramount[i] > 0:
+			recDate = rdate[i]
+			break
+	print(recDate)
+	#then plot it
+	earliest, latest = plotsingleday(recDate.year, recDate.month, recDate.day, 'b', earliest, latest)
+	return earliest, latest, recDate
+	
+def plotAllBorrels():
+	"""
+	Plots a bar graph of maximum number of people at the tuesday borrels at SSR.
+	"""
+	firstdate = dt.date(2016, 9, 9)
+	lastdate = dt.date(2017, 4, 4)
+	
+	#array containing all the tuesday dates and max values
+	tuesdaylist = []
+	tuesdaymax = []
+	#the date counter
+	datecounter = firstdate
+	
+	while datecounter <= lastdate:
+		try:
+			#loads that day and the day after (because events go on past 24:00 h)
+			slicedamount1, slicedtime1 = sliceDates(datecounter, starttime, endtime, True)
+			slicedamount2, slicedtime2 = sliceDates(datecounter, starttime, endtime, False)
+			slicedamount = np.append(slicedamount1, slicedamount2)
+			
+			tuesdaymax = np.append(tuesdaymax, np.max(slicedamount))
+			tuesdaylist = np.append(tuesdaylist, datecounter)
+		except:
+			print("Skipping date without data (" + str(datecounter) + ")")
+			
+		datecounter += dt.timedelta(days = 7)
+	
+	for i in np.arange(len(tuesdaylist)):
+		print(tuesdaylist[i], tuesdaymax[i])
+		
+	#now we plot
+	plt.bar(tuesdaylist, tuesdaymax, width = 7)
+	plt.xlabel("Datum")
+	plt.ylabel("Maximaal aantal mensen in het pand")
+	plt.title("Maximaal aantal mensen in het pand per vrijdag van dit collegejaar")
+	plt.savefig("Maximaal # mensen alle vrijdagen.png", dpi = 200)
+	plt.show()
+	
+earliest, latest, recDate = plotRecent(earliest, latest)	
+'''
+	#plot data per day
+
+#first borrels in september 2016
+#earliest, latest = plotsingleday(2016, 9, 6, '#FB07FF', earliest, latest)
+#earliest, latest = plotsingleday(2016, 9, 13, '#FF0635', earliest, latest)
+#earliest, latest = plotsingleday(2016, 9, 20, '#FF6404', earliest, latest)
+#earliest, latest = plotsingleday(2016, 9, 27, '#FFCC26', earliest, latest)
+
+#Ik week 2017
+#earliest, latest = plotsingleday(2017, 2, 13, '#02B7FF', earliest, latest)
+#earliest, latest = plotsingleday(2017, 2, 14, '#FB07FF', earliest, latest)
+#earliest, latest = plotsingleday(2017, 2, 15, '#7306FF', earliest, latest)
+#earliest, latest = plotsingleday(2017, 2, 16, '#FF060D', earliest, latest)
+#earliest, latest = plotsingleday(2017, 2, 17, '#FF8E06', earliest, latest)
+
+
+#Oud & nieuw
+#earliest, latest = plotsingleday(2016, 12, 31, 'b', earliest, latest)
+#KSF 
+#earliest, latest = plotsingleday(2016, 12, 23, 'g', earliest, latest)
+
+#random date
+recDate = dt.datetime(2017,3,7)
+earliest, latest = plotsingleday(recDate.year, recDate.month, recDate.day, 
+								'b', earliest, latest)
+
+
+
+#EL CID 2017
+#plotsingleday(2017, 8, 14, '#02B7FF', earliest, latest)
+#plotsingleday(2017, 8, 15, '#FB07FF', earliest, latest)
+#plotsingleday(2017, 8, 16, '#7306FF', earliest, latest)
+#plotsingleday(2017, 8, 17, '#FF060D', earliest, latest)
+#plotsingleday(2017, 8, 18, '#FF8E06', earliest, latest)
+
+#EL CID 2016
+#plotsingleday(2016, 8, 15, '#02B7FF', earliest, latest)
+#plotsingleday(2016, 8, 16, '#FB07FF', earliest, latest)
+#plotsingleday(2016, 8, 17, '#7306FF', earliest, latest)
+#plotsingleday(2016, 8, 18, '#FF060D', earliest, latest)
+#plotsingleday(2016, 8, 19, '#FF8E06', earliest, latest)
+plotsingleday(2016, 9, 12, '#FF8E06', earliest, latest)
+
+plotstart = np.min(earliestlist)
+plotend = np.max(latestlist)
+
+	#the plotting commands for plotting a single or multiple days
+#makes plot nicer
+sns.set_style("darkgrid", {'grid.color': '0.4', 'grid.linestyle': u':', 'legend.frameon': True})
+
+##sets the xlim 
+plt.xlim(plotstart - dt.timedelta(minutes = 30), plotend + dt.timedelta(minutes = 30))
+plt.ylim(-10, 310)
+
+plt.legend(loc = 'lower right', shadow = True).draggable()
+#plt.title('Aantal mensen te S.C.R.E.D. op ' + str(recDate.date()))
+plt.title('Aantal mensen te S.C.R.E.D. tijdens EL CID 2016')
+plt.xlabel('Tijd')
+plt.ylabel('Aantal')
+#plt.savefig("Aantal mensen te S.C.R.E.D. EL CID 2016.png", bbox_inches='tight', dpi = 200)
+plt.show()
+
+'''
+#plotAllBorrels()
---
+++ SSRteller-notification.py
@@ -0,0 +1,296 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+import seaborn as sns
+#for downloading
+from urllib.request import urlretrieve
+#for removing the downloaded file
+import os
+#for making and reading an SQL database
+import sqlite3 as lite
+#to make the program sleep
+import time
+#for displaying a notification
+import tkinter as tk
+from tkinter import ttk
+#used for model fitting
+from scipy.optimize import curve_fit
+plt.close()
+
+'''
+#start and end time of borrel data
+starttime = dt.time(20)
+endtime = dt.time(5)
+#start and end time of plot
+startPlot = dt.time(19,30)
+endPlot = dt.time(2,30)
+
+#earliest and latest start of counting
+earliest = dt.datetime.now()
+latest = dt.datetime(1900, 1, 1)
+'''
+#maximum number of people in the building
+maxnum = 300
+
+#name where the data will be downloaded
+downloadname = "./teller_log.txt"
+
+
+def downloadData():
+	#list of first and last data points for each day. From these the plotting
+	#range is determined
+	earliestlist = []
+	latestlist = []
+
+	url = "https://api.joostvansomeren.nl/teller/log.txt"
+
+	#download log
+	print("Downloading data")
+	urlretrieve(url, downloadname)
+	
+def loadData(now):
+	"""
+	Load the data of today
+	"""
+	monthlist = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+	
+	#arrays containing the data
+	dates = []
+	amount = []
+	
+	with open(downloadname, 'r') as f:
+		prevdate = dt.datetime(1900, 1, 1)
+		prevamount = 0
+		
+		for line in f:
+			#split the line using the spaces
+			sl = line.split()
+			#convert the text month to a month number
+			monthN = monthlist.index(sl[3]) + 1
+			#find the date of the line
+			tempstring = '{0}-{1}-{2} {3}'.format(monthN, sl[4], sl[5], sl[6])
+			tdatetime = dt.datetime.strptime(tempstring, '%m-%d-%Y %H:%M:%S')
+			
+			#add the date to the array if it is the correct day
+			if tdatetime.date() == now.date() or tdatetime.date() + dt.timedelta(days = 1) == now.date():
+				#check if the current time is not equal to the previous one. If so,
+				#add the sum of the last few entries
+				if (tdatetime - prevdate) > dt.timedelta(seconds = 0) and prevdate > dt.datetime(1900, 1, 1):
+					dates = np.append(dates, prevdate)
+					amount = np.append(amount, prevamount)
+					
+				prevdate = tdatetime
+				prevamount = int(sl[0])
+
+	return dates, amount
+
+def findDeriv(dates, amount, nowmoment, diffwidth = 10):
+	"""
+	Find the growth in the number of people in the building and send a warning
+	if the building is almost full.
+	
+	Input:
+		dates (1D array, datetime): array of datetimes of the data.\n
+		amount (1D array, int): array containing the amount of people in the building
+		at every moment in dates.\n
+		nowmoment (datetime): the moment defined as now. Could be different than desired
+		if looking at historical data.\n
+		diffwidth (int): length of time in minutes of which data will be aggregated to 
+		calculate the derivative.
+	"""
+	#slice the data to the last diffwidth number of minutes
+	diffbegin = 0
+	diffend = 0
+	for i in np.arange(len(dates)):
+		if dates[i] > (nowmoment - dt.timedelta(minutes = diffwidth)) and diffbegin == 0:
+			diffbegin = i			
+		if dates[i] > nowmoment and diffend == 0:
+			diffend = i
+			break
+		if i == len(dates) - 1:
+			diffend = i
+			break
+			
+	print(diffbegin, diffend)
+
+	deriv = (amount[diffend] - amount[diffbegin])/(dates[diffend] - dates[diffbegin]).seconds
+
+	#determine how long it will take until the building is full
+	fulltime = (maxnum - amount[diffend]) / deriv
+	
+	print('Capacity: {0}/{1}, full in: {2}:%02d'.format(int(amount[diffend]), maxnum, int(fulltime/60)) % int(fulltime % 60))
+	print('------------------\n')
+	
+	return deriv, fulltime, amount[diffend]
+	
+def findFullTime(dates, amount, nowmoment, diffwidth = 10):
+	"""
+	Find when the building will be full using a second- or third order approximation
+	
+	Input:
+		dates (1D array, datetime): array of datetimes of the data.\n
+		amount (1D array, int): array containing the amount of people in the building
+		at every moment in dates.\n
+		nowmoment (datetime): the moment defined as now. Could be different than desired
+		if looking at historical data.\n
+		diffwidth (int): length of time in minutes of which data will be aggregated to 
+		calculate the derivative.
+	"""
+	
+	def model(x, a, b, c, d):
+		return a*x**3 + b*x**2 + c*x + d
+		
+	#slice the data to the last diffwidth number of minutes
+	diffbegin = 0
+	diffend = 0
+	for i in np.arange(len(dates)):
+		if dates[i] > (nowmoment - dt.timedelta(minutes = diffwidth)) and diffbegin == 0:
+			diffbegin = i			
+		if dates[i] > nowmoment and diffend == 0:
+			diffend = i
+			break
+		if i == len(dates) - 1:
+			diffend = i
+			break
+	
+	#cut the data to the slice
+	cutdates = dates[diffbegin:diffend]
+	cutamount = []
+	#transform cutamount from datetime objects to number of seconds before now
+	for a in amount[diffbegin:diffend]:
+		n_s = a - (nowmoment - dt.timedelta(minutes = diffwidth))
+		print(n_s)
+	'''
+	popt, pcov = curve_fit(model, xdata, ydata, p0 = (1000., 1.))
+	#Then the standard deviation is given by:
+	perr = np.sqrt(np.diag(pcov))
+	#get the residuals:
+	residuals = ydata- model(xdata, popt)
+	#to get R^2:
+	ss_res = np.sum(residuals**2)
+	ss_tot = np.sum((ydata-np.mean(ydata))**2)
+	r_squared = 1 - (ss_res / ss_tot)
+	'''
+	
+def makeplot(dates, amount):
+	amountdiff = np.diff(amount)
+	timediff = np.diff(dates)
+	tdiff_s = []
+	for t in timediff:
+		tdiff_s = np.append(tdiff_s, float(t.seconds))
+
+	sns.set()
+
+	#plot with two axes:
+	fig, ax1 = plt.subplots()
+	#makes another y-axis
+	ax2 = ax1.twinx()
+	#plots first line
+	lns1 = ax1.plot(dates, amount, color = 'b')
+	#plots second line
+
+	#lns2 = ax2.plot(dates, np.append(amountdiff/tdiff_s, 0), color = 'r')
+	for tl1 in ax1.get_yticklabels():
+		tl1.set_color('b')
+	for tl2 in ax2.get_yticklabels():
+		tl2.set_color('r')
+	
+	plt.show()
+	
+def plotFulltime(dates, amount, diffwidth = 5):
+	"""
+	Plot the time until the building is full for an entire evening
+	"""
+	derivlist = []
+	fulltimelist = []
+	plotdates = []
+
+	for d in dates:
+		try:
+			deriv, fulltime = findDeriv(dates, amount, d, diffwidth = diffwidth)
+			derivlist = np.append(derivlist, deriv)
+			fulltimelist = np.append(fulltimelist, fulltime)
+			plotdates = np.append(plotdates, d)
+		except:
+			print('Date/time incorrect')
+			derivlist = np.append(derivlist, 0)
+			fulltimelist = np.append(fulltimelist, 0)
+			plotdates = np.append(plotdates, d)
+	
+	sns.set()
+	
+	#plot with two axes:
+	fig, ax1 = plt.subplots()
+	#makes another y-axis
+	ax2 = ax1.twinx()
+	#plots first line
+	lns1 = ax1.plot(dates, amount, color = 'b')
+	#plots second line
+	#lns2 = ax2.plot(plotdates, derivlist * 60, color = 'r')
+	lns2 = ax2.scatter(plotdates, fulltimelist / 60, color = 'r', marker = 'x')
+
+	#find where the time until full was less than 10 minutes
+	f_dates = plotdates[((fulltimelist / 60) < 10) * ((fulltimelist / 60) > 0)]
+	ax1.scatter(f_dates, np.zeros(len(f_dates)), color = 'grey')
+
+	for tl1 in ax1.get_yticklabels():
+		tl1.set_color('b')
+	for tl2 in ax2.get_yticklabels():
+		tl2.set_color('r')
+
+	ax2.set_ylabel('Time untill full [min]')
+	#ax2.set_ylabel('People per minute')
+
+	plt.show()
+
+def popupmsg(msg, title = 'Warning!'):
+	"""
+	Creates a popup message
+	"""
+	XL_FONT = ('Verdana', 30)
+	LARGE_FONT= ("Verdana", 12)
+	NORM_FONT= ("Verdana", 10)
+	SMALL_FONT= ("Verdana", 8)
+
+	popup = tk.Tk()
+	popup.wm_title(title)
+	label = ttk.Label(popup, text=msg, font=XL_FONT)
+	label.pack(side='top', fill='x', pady=20, padx = 35)
+	B1 = ttk.Button(popup, text='Okay', command = popup.destroy)
+	B1.pack()
+	popup.mainloop()
+    
+#makeplot(dates, amount)
+
+#time to wait between each run, in seconds
+waittime = 30
+
+#number of minutes that the warning should be in advance of the building being full
+warningmin = 20
+
+#initializing the amount of people in the building
+currentamount = 0
+
+while currentamount < maxnum:
+	now = dt.datetime.today()
+	#now = dt.datetime.strptime('10-31-2017 22:15:00', '%m-%d-%Y %H:%M:%S')
+
+	downloadData()
+	dates, amount = loadData(now)
+	
+	#break the loop if the data is not of today
+	if len(dates) < 1:
+		print('No data of today yet. Skipping check...')
+	else:
+		deriv, fulltime, currentamount = findDeriv(dates, amount, now)
+	
+		#print the message indicating how full the building is
+		fullmessage = 'S.C.R.E.D. at {0}/{1} capacity. Full in {2}:%02d'.format(int(currentamount), maxnum, int(fulltime/60)) % int(fulltime % 60)
+		print(fullmessage)
+
+		#show a warning when the building will be full in less than 15 minutes
+		if currentamount > 180 and (fulltime / 60) < warningmin and fulltime > 0:
+			popupmsg(fullmessage)
+		
+	#wait for a few seconds
+	time.sleep(waittime)
